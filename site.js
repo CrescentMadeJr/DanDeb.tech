7ab4c48486be86b83a1fc579c75187b8e35b29df
CrescentMadeJr <daniel.debenedetto@pm.me>
2023-03-05 20:34:03 -0500

    Added a bunch of content.  Changed my mind a few times.

--- site.js
+++ site.js
@@ -1,14 +1,15 @@
 // It's me, Dano.
-console.log("           .'\\   /`.")
-console.log("         .'.-.`-'.-.`.")
-console.log("    ..._:   .-. .-.   :_...")
-console.log("  .'   '-.(o )   (o ).-'   `.")
-console.log(" :  _    _ _`~(_)~`_ _    _  :")
-console.log(":  /:   ' .-=_   _=-. `   ;\\  :")
-console.log(":   :|-.._  '     `  _..-|:   :")
-console.log(" :   `:| |`:-:-.-:-:'| |:'   :")
-console.log("  `.   `.| | | | | | |.'   .'")
-console.log("    `.   `-:_| | |_:-'   .'")
-console.log("      `-._   ````    _.-'")
-console.log("          ``-------''")
-console.log("       We're all mad here.")
+console.log("           .'\\   /`.");
+console.log("         .'.-.`-'.-.`.");
+console.log("    ..._:   .-. .-.   :_...");
+console.log("  .'   '-.(o )   (o ).-'   `.");
+console.log(" :  _    _ _`~(_)~`_ _    _  :");
+console.log(":  /:   ' .-=_   _=-. `   ;\\  :");
+console.log(":   :|-.._  '     `  _..-|:   :");
+console.log(" :   `:| |`:-:-.-:-:'| |:'   :");
+console.log("  `.   `.| | | | | | |.'   .'");
+console.log("    `.   `-:_| | |_:-'   .'");
+console.log("      `-._   ````    _.-'");
+console.log("          ``-------''");
+console.log("       We're all mad here.");
+
