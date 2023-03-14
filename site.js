58019483413d6dddd974556d8437722c248e2012
CrescentMadeJr <daniel.debenedetto@pm.me>
2023-03-14 17:14:54 -0400

    Finally! The accordion boxes are working, if a bit ugly.

--- site.js
+++ site.js
@@ -1,15 +1,14 @@
-// It's me, Dano.
-console.log("           .'\\   /`.");
-console.log("         .'.-.`-'.-.`.");
-console.log("    ..._:   .-. .-.   :_...");
-console.log("  .'   '-.(o )   (o ).-'   `.");
-console.log(" :  _    _ _`~(_)~`_ _    _  :");
-console.log(":  /:   ' .-=_   _=-. `   ;\\  :");
-console.log(":   :|-.._  '     `  _..-|:   :");
-console.log(" :   `:| |`:-:-.-:-:'| |:'   :");
-console.log("  `.   `.| | | | | | |.'   .'");
-console.log("    `.   `-:_| | |_:-'   .'");
-console.log("      `-._   ````    _.-'");
-console.log("          ``-------''");
-console.log("       We're all mad here.");
+var acc = document.getElementsByClassName("accordion-box");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var infobox = this.nextElementSibling;
+    if (infobox.style.display === "block") {
+      infobox.style.display = "none";
+    } else {
+      infobox.style.display = "block";
+    }
+  });
+}
