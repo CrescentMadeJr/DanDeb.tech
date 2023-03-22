8b5e861482e6c5c091aac5c1b3de40a599b71fb7
CrescentMadeJr <daniel.debenedetto@pm.me>
2023-03-22 14:13:32 -0400

    Fixes for buttons and some more formatting.  Changed the main layout to use hamburger buttons and then to regular nav on wider.

--- site.js
+++ site.js
@@ -1,14 +1,17 @@
 // Daniel DeBenedetto
 // 2023-02-15
 
+// Need to fix the nav button when expanded. 
+//if classlist.contains None  -- classlist remove
+
 // Fun page accordion box
-var acc = document.getElementsByClassName("accordion-box");
-var i;
+const acc = document.getElementsByClassName("accordion-box");
+let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var infobox = this.nextElementSibling;
+    const infobox = this.nextElementSibling;
     if (infobox.style.display === "block") {
       infobox.style.display = "none";
     } else {
@@ -17,9 +20,9 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-/* Hamburger button */
+// Hamburger button 
 function toggleNav() {
-  var x = document.getElementById("navLinks");
+  const x = document.getElementById("navLinks");
   console.log(x)
   if (x.style.display === "flex") {
     x.style.display = "none";
