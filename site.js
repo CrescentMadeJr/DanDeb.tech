b30cef1dbe764be22c20a34033f682ff0b494395
CrescentMadeJr <daniel.debenedetto@pm.me>
2023-03-27 15:47:41 -0400

    Changes to be committed: 	modified:   CHANGELOG 	modified:   about.html 	modified:   contact.html 	modified:   fun.html 	modified:   index.html 	modified:   site.css 	deleted:    site.css.backup 	modified:   site.js

--- site.js
+++ site.js
@@ -4,6 +4,17 @@
 // Need to fix the nav button when expanded. 
 //if classlist.contains None  -- classlist remove
 
+// Hamburger menu 
+function toggleNav() {
+  const x = document.getElementById("navLinks");
+  console.log(x)
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+} 
+
 // Fun page accordion box
 const acc = document.getElementsByClassName("accordion-box");
 let i;
@@ -20,14 +31,27 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-// Hamburger button 
-function toggleNav() {
-  const x = document.getElementById("navLinks");
-  console.log(x)
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "flex";
-  }
-} 
+//Contact form
+const form = document.querySelector('#contact-form');
+const output = document.querySelector('#output');
 
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // Get form data
+  const formData = new FormData(form);
+  const data = {};
+  formData.forEach((value, key) => data[key] = value);
+
+  // Save data to session storage.
+  sessionStorage.setItem('contactData', JSON.stringify(data));
+
+  // Display saved data from seesion storage
+  const savedData = JSON.parse(sessionStorage.getItem('contactData'));
+  output.innerHTML = `
+  <section class="
+    <p>Name: ${savedData.fullname}</p>
+    <p>Email: ${savedData.email}</p>
+    <p>Message: ${savedData.message}</p>
+  `;
+});
